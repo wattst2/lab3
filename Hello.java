2deaaff275f245b21fd631c267e54b2f7887533e
Alioune Diagne <44923508+Alioune98@users.noreply.github.com>
2022-10-03 11:48:56 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 { 
   public static void main( String argv[] )
   {
-    System.out.println( "Hello, " + argv[0] + "!" );
+    System.out.println( "Hello, this Ali" + argv[0] + "!" );
   }
 }
